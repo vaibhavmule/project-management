3f4bbd4710fe3b7cdba397f0dca0a3101692b80e
Vaibhav Mule <vaibhavmule135@gmail.com>
2020-08-17 06:26:43 +0530

    add project view

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,25 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from app import app, mongo
+from bson import ObjectId
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    projects = mongo.db.projects.find()
-    return render_template('index.html', title='Home', projects=projects)
+	projects = mongo.db.projects.find()
+	return render_template('index.html', title='Home', projects=projects)
+
 
 @app.route('/create', methods=['GET','POST'])
 def create_project():
-    # projects = mongo.db.projects
-    mongo.db.projects.insert_one({'title': 'Project 1'})
-    return render_template('create.html')
+	if request.method == 'POST':
+		title = request.form['title']
+		mongo.db.projects.insert_one({'title': title})
+		return redirect('/')
+	return render_template('create.html')
+
+
+@app.route('/<id>')
+def project(id):	
+	project = mongo.db.projects.find_one({'_id': ObjectId(id)})
+	return render_template('project.html', project=project)

9b73b62cfd9c248371fb07ff8c6aab9b838cd476
Vaibhav Mule <vaibhavmule135@gmail.com>
2020-08-17 23:13:34 +0530

    wip auth

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, session, url_for
 from app import app, db, socketio, login_required
 from bson import ObjectId
+import bcrypt
 
 
 @app.route('/')
+@login_required
 def index():
 	projects = db.projects.find()
 	return render_template('index.html', title='Home', projects=projects)
@@ -24,13 +26,34 @@ def project(id):
 	return render_template('project.html', project=project)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		users = db.users
+		exists = users.find_one({'username' : request.form['username']})
+		if exists:
+			if bcrypt.hashpw(request.form['password'], exists['password']) == exists['password'].encode('utf-8'):
+				session['username'] = request.form['username']
+				return redirect(url_for('index'))
 	return render_template('auth/login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+	if request.method == 'POST':
+		users = db.users
+		exists = users.find_one({'username' : request.form['username']})
+		if not exists:
+			hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+			users.insert_one({
+				'username' : request.form['username'],
+				'password' : hashpass,
+				'role': int(request.form['role'])
+			})
+			session['username'] = request.form['username']
+			return redirect(url_for('index'))
+
+		return "The username already exists"
 	return render_template('auth/register.html')
 
 
